e9eb30e6d2524bf9702a2d7682822bc3ed69eff9
Ho Thanh Xuan <htxuan842@gmail.com>
2026-05-23 03:31:06 +0700

    update

--- check_bin.ps1
+++
@@ -1,7 +0,0 @@
-$Shell = New-Object -ComObject Shell.Application
-$RecycleBin = $Shell.Namespace(0xA)
-foreach($item in $RecycleBin.Items()) {
-    if ($item.Name -match '(?i)assets|images') {
-        Write-Output ($item.Name + ' | ' + $item.Path)
-    }
-}
